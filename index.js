37637a32feddfd951b49b90f191b5c11c873eb14
Matthew Scott <m4ttplu5@gmail.com>
2021-07-18 15:57:45 -0700

    init

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
+require('./utils/startup')()
 const Koa = require('koa')
 const KoaRouter = require('@koa/router')
 const yamljs = require('yamljs')
 const { koaSwagger } = require('koa2-swagger-ui')
-const startup = require('./utils/startup')
-startup()
 
 const app = new Koa()
 const router = new KoaRouter()
 const port = 2400
-
-// function * separatelyDeclaredLogic (ctx) {
-//   ctx.body = ''
-//   return `${ctx.params.id}`
-// }
 
 router.get('/', (ctx, next) => {
   try {
@@ -23,22 +17,6 @@ router.get('/', (ctx, next) => {
 })
 
 router.use('/v1', require('./v1').routes())
-
-// router.get('/:id', (ctx) => {
-//   ctx.body = `We see you ${this.params.id}`
-// })
-
-// router.get('/users/:id', separatelyDeclaredLogic)
-
-// router.get('/users', (ctx) => {
-//   ctx.body =
-// })
-
-// function * sendIdAndName () {
-//   this.body = 'id: ' + this.params.id + ' and name: ' + this.params.name
-// }
-
-// router.get('/things/:name/:id', sendIdAndName)
 
 const spec = yamljs.load('./swagger/sample.yaml')
 
