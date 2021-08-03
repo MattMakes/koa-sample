0da142ba9329264c907e0672feeb9eb0a916f085
Matthew Scott <m4ttplu5@gmail.com>
2021-08-02 22:12:24 -0700

    Updated github actions

---
+++ .github/workflows/make.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Generating docs..."
+
+# Bundle docs into zero-dependency HTML file
+npx redoc-cli bundle ../../swagger/sample.yaml && \
+mkdir dist && \
+mv redoc-static.html ./dist/index.html && \
+echo "Changed name from redoc-static.html to index.html" && \
+echo -e "\nDone!"
