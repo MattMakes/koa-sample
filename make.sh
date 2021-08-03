5a23dafc04166e81f615270a161c02a4748a3b91
Matthew Scott <m4ttplu5@gmail.com>
2021-08-02 22:14:02 -0700

    another try

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@
 echo "Generating docs..."
 
 # Bundle docs into zero-dependency HTML file
-npx redoc-cli bundle ../../swagger/sample.yaml && \
+npx redoc-cli bundle ./swagger/sample.yaml && \
 mkdir dist && \
 mv redoc-static.html ./dist/index.html && \
 echo "Changed name from redoc-static.html to index.html" && \
